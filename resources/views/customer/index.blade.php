aa6ebf25b3bfc9e084abeb1a616eb08dd996e16b
MeliskaYI <meliskayaffaivana725myip29@gmail.com>
2021-10-24 18:07:32 +0700

    fix

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -5,19 +5,12 @@
 <br>
 <br>
 <!-- ***** Our Classes Start ***** -->
+
 <div class="card-deck">     
   @foreach($service as $serv)
-<<<<<<< HEAD
 
-=======
->>>>>>> 1c92386023804d8efdfd983d87f53da3bed15edd
   <div class="card">
-    <div class="card-header">
-      <h3>Our Service</h3>
-      <h3>Our Service</h3>
-    </div>
-<<<<<<< HEAD
-=======
+    
     <div class="card-body">
           <h4>{{ $serv->user->username }}</h4>    
           <h4>{{ $serv->service_name }}</h4>
@@ -28,7 +21,6 @@
 				<a href="{{route ('service.show', $serv->ID_service)}}" class="card-link">Lihat</a>
 			</div>
     </div>
->>>>>>> 1c92386023804d8efdfd983d87f53da3bed15edd
   </div>
       @endforeach
 </div>
